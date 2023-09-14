b125c06cc7e6fcdbbb18ad5dfa5d8c7f08ce2f45
EduardYan <eduarygp@gmail.com>
2023-09-14 03:04:51 +0200

    Bootswatch added using npm

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,3 +3,12 @@ AOS.init({
   easing: "ease-out-back",
   duration: 1100,
 });
+
+//starting carousel
+document.addEventListener("DOMContentLoaded", () => {
+  const myCarousel = document.getElementById("carousel");
+  const carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000, 
+    wrap: true,
+  });
+});
